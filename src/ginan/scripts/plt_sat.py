ca88eb74be946a35e95b708654786a2393b5ceac
Sebastien Allgeyer <sebastien.allgeyer@gmail.com>
2023-02-07 23:57:11 +1100

    splitting function in read/plot

--- src/ginan/scripts/plt_sat.py
+++ src/ginan/scripts/plt_sat.py
@@ -7,13 +7,16 @@ from ginan.data.satellite import satellite
 from ginan.dbconnector import mongo
 
 plt.style.use(['seaborn-ticks', 'seaborn-paper'])
-def plot(args):
+def read(args):
     db = mongo.MongoDB(url=args.db, data_base=args.coll)
     satG01 = satellite(db, sat=args.sat)
     satG01.get_postfit()
     rms = satG01.get_rms()
     print(f"{args.coll} {arg.sat}   {np.array2string(rms[:3], precision=6, floatmode='fixed')}  => {rms[3]:.6f}")
     satG01.get_state()
+    return satG01
+
+def plot(args, satG01):
     fig, ax = plt.subplots(nrows=3)
     if (args.to_rac):
         r = satG01.rac()
@@ -28,7 +31,9 @@ def plot(args):
         a.set_ylabel(l)
     plt.savefig(f"plt_{args.coll}_{args.sat}.pdf", bbox_inches='tight')
 
-
+def main(arg):
+    orbit = read(arg)
+    plot(arg, orbit)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -41,4 +46,4 @@ if __name__ == "__main__":
     parser.add_argument('-r', '--residual', action='store_true', default=False)
     parser.add_argument('--to_rac', action='store_true', default=False)
     arg = parser.parse_args()
-    plot(arg)
+    main(arg)
