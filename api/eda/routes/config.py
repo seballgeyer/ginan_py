3afa751f62cb1f18b0beb0eed45cf4217806259e
Sebastien Allgeyer <sebastien.allgeyer@gmail.com>
2023-06-23 15:38:16 +1000

    fix config

--- api/eda/routes/config.py
+++ api/eda/routes/config.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, session
 from flask import current_app
 from sateda.dbconnector.mongo import MongoDB
 from . import eda_bp
-
+from ..utilities import init_page, extra
 # eda_bp = Blueprint('eda', __name__)
 
 # config_bp = Blueprint("config", __name__)
@@ -10,7 +10,19 @@ from . import eda_bp
 
 @eda_bp.route("/config", methods=["GET", "POST"])
 def config():
-    with MongoDB(session["mongo_ip"], data_base=session["mongo_db"], port=session["mongo_port"]) as client:
+    if request.method == "POST":
+        return handle_post_request()
+    else:
+        return init_page(template="config.jinja")
+
+
+
+def handle_post_request():
+    form = request.form
+    database = form.get("database")
+    print(database)
+    with MongoDB(session["mongo_ip"], data_base=database, port=session["mongo_port"]) as client:
         config = client.get_config()
+    print(config)
     config["_id"] = str(config["_id"])
-    return render_template("config.jinja", config=config)
+    return render_template("config.jinja", configuration=config, selection=form)
