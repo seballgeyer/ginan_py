2c233deec5f19f00e242cd132187613b3b9b22e8
Sebastien Allgeyer <sebastien.allgeyer@gmail.com>
2023-06-27 19:33:16 +1000

    test EOP

--- src/sateda/io/eop/eop.py
+++ src/sateda/io/eop/eop.py
@@ -41,7 +41,7 @@ class EOP:
             self.dX = self.C04[:, 8]
             self.dY = self.C04[:, 9]
     
-    def interpolate(self, mjd: float, degree: int) -> None:
+    def interpolate_lagrange(self, mjd: float, degree: int) -> None:
         #interpolate the EOP variables to the given mjd using a polynomial interpolation of degree defined by the user
         locate = np.searchsorted(self.mjd, mjd, side='left')
         mjd_ = self.mjd[locate-degree:locate+degree]
@@ -58,9 +58,23 @@ class EOP:
         for i, data in enumerate([self.x, self.y, self.ut1_utc, self.lod, self.dX, self.dY]):
             result[i] = np.interp(mjd, self.mjd[locate-1:locate+1], data[locate-1:locate+1])
         return result
-        
-        
-            
+    
+    def interpolate_neville(self, mjd, degree):
+        locate = np.searchsorted(self.mjd, mjd, side='left')
+        center = locate - degree // 2  # Calculate the center index
+        result = np.zeros((6,))
+        for i, data in enumerate([self.x, self.y, self.ut1_utc, self.lod, self.dX, self.dY]):
+            n = degree + 1
+            Q = np.zeros((n, n))
+            Q[:, 0] = data[center : center + n]
+            for j in range(1, n):
+                for k in range(n - j):
+                    Q[k, j] = ((mjd - self.mjd[center + k]) * Q[k + 1, j - 1] - (mjd - self.mjd[center + k + j]) * Q[k, j - 1]) \
+                                / (self.mjd[center + k + j] - self.mjd[center + k])
+            result[i] = Q[0, degree]
+        return result
+    
+                
             
 if __name__ == "__main__":
     eop = EOP()
@@ -69,14 +83,17 @@ if __name__ == "__main__":
     date = np.arange(37667.25, 37670.75, 1/(24*60))
     lagrange_data = np.zeros((len(date), 6))
     linear_data = np.zeros((len(date), 6))
+    neuville_data = np.zeros((len(date), 6))
     for i, d in enumerate(date):
         # print(d)
-        lagrange_data[i, :] = eop.interpolate(d, 3)
+        lagrange_data[i, :] = eop.interpolate_lagrange(d, 3)
+        neuville_data[i, :] = eop.interpolate_neville(d, 3)
         linear_data[i, :] = eop.interpolate_linear(d)
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots(nrows=6, ncols=2, sharex=True)
     for i in range(6):
         ax[i,0].plot(date, lagrange_data[:, i])
-        ax[i,0].plot(date, linear_data[:, i])
-        ax[i,1].plot(date, lagrange_data[:, i] - linear_data[:, i])
+        ax[i,0].plot(date, neuville_data[:, i])
+        ax[i,1].plot(date, lagrange_data[:, i] - neuville_data[:, i])
+
     plt.show()
