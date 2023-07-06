a1a539f120b1447384d9d35c876a803fa057d782
Sebastien Allgeyer <sebastien.allgeyer@gmail.com>
2023-07-06 16:47:51 +1000

    linter + docs

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+TestPositions Tests for the Position class.
+"""
 import unittest
 import datetime
 import numpy as np
@@ -10,34 +14,40 @@ class TestPositions(unittest.TestCase):
     """
     def test_diff(self):
         """
-        test_rotate_enu Test the rotation function. 
+        test_rotate_enu Test the differencing function. 
+        For to positions, the difference should be the same as the difference.
+        Extra complexity added in it as the 2 data does not have the same time stamps.
         """
         reference = MeasurementArray()
         data  = MeasurementArray()
         time0 = datetime.datetime(2021, 1, 1, 0, 0, 0)
         data_dict = {
             "_id": {"sat": "", "site": "North"},
-            "t": [time0, time0+datetime.timedelta(seconds=1)],
-            "x_0": [0, 0],
-            "x_1": [0, 0],
-            "x_2": [1, 1],
+            "t": [time0, time0+datetime.timedelta(seconds=1), time0+datetime.timedelta(seconds=2)],
+            "x_0": [0, 0, 0],
+            "x_1": [0, 0, 0],
+            "x_2": [1, 0, -1],
         }
         reference.append(Measurements.from_dictionary(data_dict))
         data_dict = {
             "_id": {"sat": "", "site": "North"},
-            "t": [time0, time0+datetime.timedelta(seconds=1)],
-            "x_0": [1,1],
-            "x_1": [1, 1],
-            "x_2": [2, 3],
+            "t": [time0-datetime.timedelta(seconds=1), time0, time0+datetime.timedelta(seconds=1)],
+            "x_0": [1, 1, 1],
+            "x_1": [1, 1, 1],
+            "x_2": [2, 3, 5],
         }
         data.append(Measurements.from_dictionary(data_dict))
         pos = Position(data=data, base=reference)
-        self.assertTrue(np.alltrue(pos.data.arr[0].data["x_2"] == [1,2]))
+        self.assertEqual(len(pos.data.arr[0].data["x_2"]), 2)
+        self.assertEqual(pos.data.arr[0].epoch[0], time0)
+        self.assertEqual(pos.data.arr[0].epoch[1], time0+datetime.timedelta(seconds=1))
+        self.assertTrue(np.alltrue(pos.data.arr[0].data["x_2"] == [2,5]))
 
 
     def test_rotate_north(self):
         """
-        test_rotate_enu Test the rotation function. 
+        test_rotate_enu Test the rotation function. For a point at the North Pole. 
+        The East and North are difficult to define, all the Z (x_2) is going in Up.
         """
         reference = MeasurementArray()
         data  = MeasurementArray()
@@ -65,13 +75,14 @@ class TestPositions(unittest.TestCase):
 
     def test_rotate_lat0lon0(self):
         """
-        test_rotate_enu Test the rotation function. 
+        test_rotate_enu Test the rotation function for a point at lat=0 lon=0.
+        x_1 (Y) is going in East, x_3 (Z) in North and x_0 (X) in Up. 
         """
         reference = MeasurementArray()
         data  = MeasurementArray()
         time0 = datetime.datetime(2021, 1, 1, 0, 0, 0)
         data_dict = {
-            "_id": {"sat": "", "site": "North"},
+            "_id": {"sat": "", "site": "Eq0"},
             "t": [time0, time0+datetime.timedelta(seconds=1)],
             "x_0": [1,1],
             "x_1": [0, 0],
@@ -79,31 +90,31 @@ class TestPositions(unittest.TestCase):
         }
         reference.append(Measurements.from_dictionary(data_dict))
         data_dict = {
-            "_id": {"sat": "", "site": "North"},
+            "_id": {"sat": "", "site": "Eq0"},
             "t": [time0, time0+datetime.timedelta(seconds=1)],
             "x_0": [1.1,1.1],
             "x_1": [0.2, 0.2],
             "x_2": [0.3, 0.3],
-            
         }
         data.append(Measurements.from_dictionary(data_dict))
         pos = Position(data=data, base=reference)
         pos.rotate_enu()
-        
-        self.assertTrue(np.allclose(pos.data.arr[0].data["x_2"] , [0.1,0.1], atol=1e-15))
-        self.assertTrue(np.allclose(pos.data.arr[0].data["x_1"] , [0.3,0.3], atol=1e-15))
-        self.assertTrue(np.allclose(pos.data.arr[0].data["x_0"] , [0.2,0.2], atol=1e-15))
-        
-        
+
+        self.assertTrue(np.allclose(pos.data.arr[0].data["x_2"] , [0.1,0.1], atol=1e-15)) #U
+        self.assertTrue(np.allclose(pos.data.arr[0].data["x_1"] , [0.3,0.3], atol=1e-15)) #N
+        self.assertTrue(np.allclose(pos.data.arr[0].data["x_0"] , [0.2,0.2], atol=1e-15)) #E
+
+
     def test_rotate_lat0lon90(self):
         """
-        test_rotate_enu Test the rotation function. 
+        test_rotate_enu Test the rotation function for a point at lat=0 lon=90.
+        x_1 (Y) is going in Up, x_3 (Z) in North and x_0 (X) in -East. 
         """
         reference = MeasurementArray()
         data  = MeasurementArray()
         time0 = datetime.datetime(2021, 1, 1, 0, 0, 0)
         data_dict = {
-            "_id": {"sat": "", "site": "North"},
+            "_id": {"sat": "", "site": "Eq90"},
             "t": [time0, time0+datetime.timedelta(seconds=1)],
             "x_0": [0, 0],
             "x_1": [1,1],
@@ -111,7 +122,7 @@ class TestPositions(unittest.TestCase):
         }
         reference.append(Measurements.from_dictionary(data_dict))
         data_dict = {
-            "_id": {"sat": "", "site": "North"},
+            "_id": {"sat": "", "site": "Eq90"},
             "t": [time0, time0+datetime.timedelta(seconds=1)],
             "x_0": [0.1, 0.1],
             "x_1": [1.2, 1.2],
@@ -120,7 +131,7 @@ class TestPositions(unittest.TestCase):
         data.append(Measurements.from_dictionary(data_dict))
         pos = Position(data=data, base=reference)
         pos.rotate_enu()
-        
-        self.assertTrue(np.allclose(pos.data.arr[0].data["x_2"] , [0.2,0.2], atol=1e-15))
-        self.assertTrue(np.allclose(pos.data.arr[0].data["x_1"] , [0.3,0.3], atol=1e-15))
-        self.assertTrue(np.allclose(pos.data.arr[0].data["x_0"] , [-0.1,-0.1], atol=1e-15))
+
+        self.assertTrue(np.allclose(pos.data.arr[0].data["x_2"] , [0.2,0.2], atol=1e-15))  #U
+        self.assertTrue(np.allclose(pos.data.arr[0].data["x_1"] , [0.3,0.3], atol=1e-15))  #N
+        self.assertTrue(np.allclose(pos.data.arr[0].data["x_0"] , [-0.1,-0.1], atol=1e-15)) #E
