ad0fef53e93247ac542c35c281f966d8bc2bbc81
Sebastien Allgeyer <sebastien.allgeyer@gmail.com>
2023-08-13 08:57:56 +1000

    fixing unittest (assert true==false)

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,18 +1,18 @@
 import unittest
 
+import numpy as np
+
 from sateda.core.time import Time
 
 class TestTime(unittest.TestCase):
     def setUp(self) -> None:
         self.time = Time.from_components(2007,1,1,0,0,0,0)
     def test_something(self):
-        print()
-        print(self.time)
-        print(self.time.to_utc())
-        print(self.time.to_tt())
-        print(self.time.to_tai())
-        print(self.time.to_utc().to_mjd())
-        self.assertEqual(True, False)  # add assertion here
+        self.assertTrue(self.time.time == np.datetime64("2007-01-01T00:00:00.000000000"))
+        self.assertTrue(self.time.to_utc().time == np.datetime64("2006-12-31T23:59:46.0000000"))
+        self.assertTrue(self.time.to_tt().time == np.datetime64("2007-01-01T00:00:51.1840000"))
+        self.assertTrue(self.time.to_tai().time == np.datetime64("2007-01-01T00:00:19.0000000"))
+
 
 
 if __name__ == '__main__':
