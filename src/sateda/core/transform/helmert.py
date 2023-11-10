21bb5cebf0e4c7c6f49a9a34965156828285bc53
Sebastien Allgeyer <sebastien.allgeyer@gmail.com>
2023-11-10 09:48:18 +0000

    finalysing the helmert class

--- src/sateda/core/transform/helmert.py
+++ src/sateda/core/transform/helmert.py
@@ -1,7 +1,7 @@
 from typing import List
 
 import numpy as np
-from scipy.optimize import least_squares
+
 
 class HelmertTransform:
     """
@@ -13,17 +13,25 @@ class HelmertTransform:
     :param rotation: The rotation vector, as a list of three angles in degrees. Default is [0, 0, 0].
 
     """
-    def __init__(self, scale: float = 0.0, angle: List[float] = None, translation: List[float] = None, scaling_factor: float = 1.0, degrees: bool = True):
-        self.scale_factor = scaling_factor
-        self.scale = scale * scaling_factor
-        self.angle =np.array(angle or [0, 0, 0], dtype=np.float64)
-        if degrees:
-            self.angle = np.deg2rad(self.angle)
+
+    def __init__(
+        self, scale: float = 0.0, rotation: List[float] = None, translation: List[float] = None, config: dict = None
+    ):
+        self.scale = scale
+        self.rotation = np.array(rotation or [0, 0, 0], dtype=np.float64)
         self.translation = np.array(translation or [0, 0, 0], dtype=np.float64)
+        if config is None:
+            config = {}
+        degrees = config.get("degrees", True)
+        self.scaling_factor = config.get("scaling_factor", 1.0)
+        self.scale *= self.scaling_factor
+        if degrees:
+            self.rotation = np.deg2rad(self.rotation)
 
     def __str__(self) -> str:
-        np.set_printoptions(precision=4, suppress=True, formatter={'float': '{:0.4e}'.format})
-        return f"HelmertTransform(scale={self.scale/self.scale_factor:.4e}, rotation={np.rad2deg(self.angle)}, translation={self.translation})"
+        np.set_printoptions(precision=4, suppress=True, formatter={"float": "{:0.4e}".format})
+        return f"HelmertTransform(scale={self.scale/self.scaling_factor:.4e}, " \
+               f"rotation={np.rad2deg(self.rotation)}, translation={self.translation})"
     
     def get_params(self) -> np.array:
         """
@@ -32,7 +40,7 @@ class HelmertTransform:
         :return: The transformation parameters.
         :rtype: numpy.array
         """
-        return np.concatenate((self.translation, [self.scale], self.angle))
+        return np.concatenate((self.translation, [self.scale], self.rotation))
 
     def as_rotation_matrix(self) -> np.array:
         """
@@ -41,8 +49,8 @@ class HelmertTransform:
         :return: The rotation matrix, as a numpy array of shape (3, 3).
         :rtype: numpy.array
         """
-        cos_angle = np.cos(self.angle)
-        sin_angle = np.sin(self.angle)
+        cos_angle = np.cos(self.rotation)
+        sin_angle = np.sin(self.rotation)
         rot_x = np.array([[1, 0, 0], [0, cos_angle[0], -sin_angle[0]], [0, sin_angle[0], cos_angle[0]]])
         rot_y = np.array([[cos_angle[1], 0, sin_angle[1]], [0, 1, 0], [-sin_angle[1], 0, cos_angle[1]]])
         rot_z = np.array([[cos_angle[2], -sin_angle[2], 0], [sin_angle[2], cos_angle[2], 0], [0, 0, 1]])
@@ -52,15 +60,15 @@ class HelmertTransform:
         """
         jac_rotation the jacobian of the rotation matrix corresponding to the rotation vector.
         """
-        cos_angle = np.cos(self.angle)
-        sin_angle = np.sin(self.angle)
-        rot_x = np.array([[1, 0, 0], [0, cos_angle[0], -sin_angle[0]], [0, sin_angle[0], cos_angle[0]]])
-        rot_y = np.array([[cos_angle[1], 0, sin_angle[1]], [0, 1, 0], [-sin_angle[1], 0, cos_angle[1]]])
-        rot_z = np.array([[cos_angle[2], -sin_angle[2], 0], [sin_angle[2], cos_angle[2], 0], [0, 0, 1]])
-        rot_x_jac = np.array([[0, 0, 0], [0, -sin_angle[0], -cos_angle[0]], [0, cos_angle[0], -sin_angle[0]]])
-        rot_y_jac = np.array([[-sin_angle[1], 0, cos_angle[1]], [0, 0, 0], [-cos_angle[1], 0, -sin_angle[1]]])
-        rot_z_jac = np.array([[-sin_angle[2], -cos_angle[2], 0], [cos_angle[2], -sin_angle[2], 0], [0, 0, 0]])
-        return rot_z @ rot_y @ rot_x_jac,  rot_z @ rot_y_jac @ rot_x, rot_z_jac @ rot_y @ rot_x 
+        cos_angle = np.cos(self.rotation)
+        sin_angle = np.sin(self.rotation)
+        rot_x = np.array([[1, 0, 0], [0, cos_angle[0], sin_angle[0]], [0, -sin_angle[0], cos_angle[0]]])
+        rot_y = np.array([[cos_angle[1], 0, -sin_angle[1]], [0, 1, 0], [sin_angle[1], 0, cos_angle[1]]])
+        rot_z = np.array([[cos_angle[2], sin_angle[2], 0], [-sin_angle[2], cos_angle[2], 0], [0, 0, 1]])
+        rot_x_jac = np.array([[0, 0, 0], [0, -sin_angle[0], cos_angle[0]], [0, -cos_angle[0], -sin_angle[0]]])
+        rot_y_jac = np.array([[-sin_angle[1], 0, -cos_angle[1]], [0, 0, 0], [cos_angle[1], 0, sin_angle[1]]])
+        rot_z_jac = np.array([[-sin_angle[2], cos_angle[2], 0], [-cos_angle[2], -sin_angle[2], 0], [0, 0, 0]])
+        return rot_z @ rot_y @ rot_x_jac, rot_z @ rot_y_jac @ rot_x, rot_z_jac @ rot_y @ rot_x
 
     def apply(self, data: np.array) -> np.array:
         """
@@ -72,9 +80,9 @@ class HelmertTransform:
         :rtype: numpy.array
         """
         data = np.array(data, dtype=np.float64)
-        return (1+self.scale) * (data @ self.as_rotation_matrix().T) + self.translation
+        return (1 + self.scale) * (data @ self.as_rotation_matrix().T) + self.translation
 
-    def jacobian(self, data: np.array, translation=True, angle=True, scale=True) -> np.array:
+    def jacobian(self, data: np.array, params=None) -> np.array:
         """
         Return the Jacobian matrix of the Helmert transformation.
 
@@ -83,34 +91,35 @@ class HelmertTransform:
         :return: The Jacobian matrix, as a numpy array of shape (7, n, 3).
         :rtype: numpy.array
         """
+        if params is None:
+            params = {"translation": True, "rotation": True, "scale": True}
         data = np.array(data, dtype=np.float64)
         rot_jac = self.jac_rotation()
         rot = self.as_rotation_matrix()
         translation_jac = np.eye(3)
         n_param = 0
-        if translation:
+        if params["translation"]:
             n_param += 3
-        if angle:
+        if params["rotation"]:
             n_param += 3
-        if scale:
+        if params["scale"]:
             n_param += 1
-        jacobian = np.zeros(( data.shape[0], 3, n_param))
+        jacobian = np.zeros((data.shape[0], 3, n_param))
         print(jacobian.shape)
-        idx=0
-        if translation:
-            jacobian[:,:,:3] = np.tile(translation_jac, (data.shape[0], 1, 1))
-            idx+=3
-        if scale:
-            jacobian[:,:,idx] = data @ rot.T
-            idx+=1
-    
-        if angle:
-            jacobian[:, :, idx] = data @ rot_jac[0].T * (1+self.scale)
-            jacobian[:, :, idx+1] = data @ rot_jac[1].T * (1+self.scale)
-            jacobian[:, :, idx+2] = data @ rot_jac[2].T * (1+self.scale)
+        idx = 0
+        if params["translation"]:
+            jacobian[:, :, :3] = np.tile(translation_jac, (data.shape[0], 1, 1))
+            idx += 3
+        if params["scale"]:
+            jacobian[:, :, idx] = data @ rot
+            idx += 1
+        if params["rotation"]:
+            jacobian[:, :, idx] = data @ rot_jac[0] * (1 + self.scale)
+            jacobian[:, :, idx + 1] = data @ rot_jac[1] * (1 + self.scale)
+            jacobian[:, :, idx + 2] = data @ rot_jac[2] * (1 + self.scale)
         return jacobian
-    
-    def fit(self, data: np.array, target: np.array, translation:bool=True, angle: bool=True, scale: bool=True) -> np.array:
+
+    def fit(self, data: np.array, target: np.array, params: dict = None) -> np.array:
         """
         Fit the Helmert transformation to a set of coordinates.
 
@@ -118,25 +127,24 @@ class HelmertTransform:
         :type data: numpy.array
         :param target: The target coordinates, as a numpy array of shape (n, 3).
         :type target: numpy.array
-        :param method: The fitting method to use. Default is 'lm'.
-        :type method: str
-        :param kwargs: Additional keyword arguments to pass to the fitting method.
+        :param params: A dictionary containing the parameters to fit. Default is {'translation': True, 'rotation': True, 'scale': True}.
+        :type params: dict
         :return: The fitted transformation parameters, as a numpy array of shape (7,).
         :rtype: numpy.array
         """
+        if params is None:
+            params = {"translation": True, "rotation": True, "scale": True}
         residuals = self.apply(data) - target
-        design = self.jacobian(data, translation=translation, angle=angle, scale=scale)
+        design = self.jacobian(data, params)
         residuals = residuals.reshape(-1)
-        print(design.shape)
-        design = design.reshape((design.shape[0]*design.shape[1], -1))
-        print(design.shape)
+        design = design.reshape((design.shape[0] * design.shape[1], -1))
         delta = np.linalg.inv(design.transpose() @ design) @ design.transpose() @ residuals
         idx = 0
-        if translation:
+        if params["translation"]:
             self.translation -= delta[:3]
             idx += 3
-        if scale:
+        if params["scale"]:
             self.scale -= delta[idx]
             idx += 1
-        if angle:
-            self.angle -= delta[idx:]
+        if params["rotation"]:
+            self.rotation -= delta[idx:]
--- tests/test_helmert.py
+++ tests/test_helmert.py
@@ -26,55 +26,64 @@ class TestHelmert(unittest.TestCase):
         self.assertTrue(np.allclose(helmert.apply(vector), expected))
 
     def test_helmert_rotation_z(self):
-        helmert = HelmertTransform(angle=[0, 0, 90])
+        helmert = HelmertTransform(rotation=[0, 0, 90])
         vector = np.array([1, 0, 0])
         expected = np.array([0, 1, 0])
         self.assertTrue(np.allclose(helmert.apply(vector), expected))
 
     def test_helmert_rotation_x(self):
-        helmert = HelmertTransform(angle=[90, 0, 0])
+        helmert = HelmertTransform(rotation=[90, 0, 0])
         vector = np.array([0, 0, 1])
         expected = np.array([0, -1, 0])
         self.assertTrue(np.allclose(helmert.apply(vector), expected))
 
     def test_helmert_rotation_x_multiple(self):
-        helmert = HelmertTransform(angle=[90, 0, 0])
+        helmert = HelmertTransform(rotation=[90, 0, 0])
         vector = np.array([[0, 0, 1], [0, 1, 0]])
         expected = np.array([[0, -1, 0], [0, 0, 1]])
         self.assertTrue(np.allclose(helmert.apply(vector), expected))
         
     def test_fit_translation(self):
-        helmert = HelmertTransform(translation=[1e-6, 2e-6, 3e-6], angle=[0, 0, 0], degrees=True)
+        helmert = HelmertTransform(translation=[1e-6, 2e-6, 3e-6], rotation=[0, 0, 0], 
+                                   config={'degrees':True})
         vector = np.random.rand(100, 3)
         target = helmert.apply(vector)
         helmert2 = HelmertTransform()
         for _i in range(10):
-            helmert2.fit(vector, target, translation=True, angle=False, scale=False)
+            helmert2.fit(vector, target, 
+                         params={'translation':True, 'rotation':False, 'scale':False})
         self.assertTrue(np.all(helmert.get_params() - helmert2.get_params() < 1e-16))
 
     def test_fit_scale(self):
-        helmert = HelmertTransform(translation=[0,0,0], angle=[0, 0, 0], scale=5.2e-5, degrees=True)
+        helmert = HelmertTransform(translation=[0,0,0], rotation=[0, 0, 0], scale=5.2e-5,
+                                    config={'degrees':True})
         vector = np.random.rand(100, 3)
         target = helmert.apply(vector)
         helmert2 = HelmertTransform()
         for _i in range(10):
-            helmert2.fit(vector, target, translation=False, angle=False, scale=True)
+            helmert2.fit(vector, target,
+                        params={'translation':False, 'rotation':False, 'scale':True})
         self.assertTrue(np.all(helmert.get_params() - helmert2.get_params() < 1e-16))
         
     def test_fit_rot(self):
-        helmert = HelmertTransform(translation=[0,0,0], angle=[1/3600.0, 2/3600.0, 0], scale=0, degrees=False)
+        helmert = HelmertTransform(translation=[0,0,0], rotation=[1/3600.0, 2/3600.0, 0], scale=0,
+                                   config={'degrees':True})
+
         vector = np.random.rand(100, 3)
         target = helmert.apply(vector)
         helmert2 = HelmertTransform()
         for _i in range(10):
-            helmert2.fit(vector, target, translation=False, angle=True, scale=False)
+            helmert2.fit(vector, target,
+                         params={'translation':False, 'rotation':True, 'scale':False})
         self.assertTrue(np.all(helmert.get_params() - helmert2.get_params() < 1e-16))
         
     def test_fit_combined(self):
-        helmert = HelmertTransform(translation=[1e-3,1e-4,2e-3], angle=[1/3600.0, 2/3600.0, 4/3600.0], scale=1e-6, degrees=False)
+        helmert = HelmertTransform(translation=[1e-3,1e-4,2e-3], rotation=[1/3600.0, 2/3600.0, 4/3600.0], scale=1e-6,
+                                    config={'degrees':True})
         vector = np.random.rand(100, 3)
         target = helmert.apply(vector)
         helmert2 = HelmertTransform()
         for _i in range(10):
-            helmert2.fit(vector, target, translation=True, angle=True, scale=True)
-        self.assertTrue(np.all(helmert.get_params() - helmert2.get_params() < 1e-16))
+            helmert2.fit(vector, target)
+        print(helmert.get_params() - helmert2.get_params())
+        self.assertTrue(np.all(helmert.get_params() - helmert2.get_params() < 1e-15))
